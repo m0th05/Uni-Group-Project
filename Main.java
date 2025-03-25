b4c24855b0630b3931c78396e203224c0f6dc2ce
m0th05 <dominiksarnowicz@gmail.com>
2025-03-25 11:44:21 +0000

    added in test files

--- Main.java
+++ Main.java
@@ -9,25 +9,59 @@ public class Main extends JFrame{
     private ArrayList<String> sales;
     private int initInventory;
     private int endInventory;
-    String[] items = {"Test 1", "Test 2"};
+    private final CardLayout cardLayout;
+    private final JPanel cardPanel;
+    String[] items = {"Sales", "Inventory", "Report"};
 
 
     public Main() {
         setTitle("Store management");
-        setLayout(new FlowLayout(5, 30, 10));
+        setLayout(new BorderLayout());
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        JPanel sales = makeSalesPage();
+        JPanel inventory = makeInventoryPage();
+        JPanel report = makeReportPage();
+
+        cardPanel.add(sales, "Sales");
+        cardPanel.add(inventory, "Inventory");
+        cardPanel.add(report,"Report");
+
         JComboBox<String> dropdown = new JComboBox<>(items);
-        add(dropdown);
-        setSize(400, 350);
+        dropdown.addActionListener(_ -> {
+            String selectedItem = (String) dropdown.getSelectedItem();
+            cardLayout.show(cardPanel,selectedItem);
+        });
+
+        add(dropdown, BorderLayout.NORTH);
+        add(cardPanel, BorderLayout.CENTER);
+
+
+
+
+
+        setSize(500, 500);
         setVisible(true);
     }
 
+    private JPanel makeSalesPage() {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("sales panel"));
+        return panel;
+    }
 
+    private JPanel makeInventoryPage() {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("inventory panel"));
+        return panel;
+    }
 
-
-
-
-
-
+    private JPanel makeReportPage() {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("report panel"));
+        return panel;
+    }
 
 
     public static void main(String[] args) {new Main();}
